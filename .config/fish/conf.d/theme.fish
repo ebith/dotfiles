6541478dc069420f8fa897f234399f372be79811
ebith <ebith.h@gmail.com>
2020-09-23 01:04:33 +0900

    change theme

---
+++ .config/fish/conf.d/theme.fish
@@ -0,0 +1,26 @@
+# Base16 Default Dark
+set fish_color_normal normal
+set fish_color_command a1b56c
+set fish_color_quote f7ca88
+set fish_color_redirection d8d8d8
+set fish_color_end ba8baf
+set fish_color_error ab4642
+set fish_color_selection white --bold --background=brblack
+set fish_color_search_match bryellow --background=brblack
+set fish_color_history_current --bold
+set fish_color_operator 7cafc2
+set fish_color_escape 86c1b9
+set fish_color_cwd green
+set fish_color_cwd_root red
+set fish_color_valid_path --underline
+set fish_color_autosuggestion 585858
+set fish_color_user brgreen
+set fish_color_host normal
+set fish_color_cancel -r
+set fish_pager_color_completion normal
+set fish_pager_color_description B3A06D yellow
+set fish_pager_color_prefix white --bold --underline
+set fish_pager_color_progress brwhite --background=cyan
+set fish_color_match 7cafc2
+set fish_color_comment f7ca88
+set fish_color_param d8d8d8
